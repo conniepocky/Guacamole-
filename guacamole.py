0248072cb742480d85871398e7cf58f56ee85d76
Connie <c.amer@icloud.com>
2019-09-03 15:13:39 +0100

    GUACAMOLE

---
+++ guacamole.py
@@ -0,0 +1,4 @@
+import os
+
+
+os.system("say Form the avacodo, form, form the avacado Form the avacodo, form, form the avacado   ,peel the avacado, peel, peel the avacodo ,peel the avacado, peel, peel the avacodo, GUACAMOLE GUA GUA CAMOLE THE GUACOMLE!")
